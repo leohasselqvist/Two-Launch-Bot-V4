4e310f4617d3082c2d24892f32fd46bfb491f2ef
Tezlz <tezlztwitter@gmail.com>
2021-07-23 04:04:30 +0200

    Lade in Test kommando med choice och options

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from discord import Client, Intents, Embed
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_option, create_choice
+from discord_slash.model import SlashCommandOptionType
 import db
 import os
 
 print("Loading...\n--------\n")
+
+"""
+SUB_COMMAND 1
+
+SUB_COMMAND_GROUP 2
+
+STRING 3
+
+INTEGER 4
+
+BOOLEAN 5
+
+USER 6
+
+CHANNEL 7
+
+ROLE 8
+"""
 
 token = open("client_secret.txt", "r").read()
 
@@ -33,9 +53,34 @@ async def on_ready():
 #     client.reload_extension(f'cogs.{extension}')
 
 
-@slash.slash(name="ping")
-async def _ping(ctx):  # Defines a new "context" (ctx) command called "ping."
-    await ctx.send(f"Pong! ({bot.latency*1000}ms)")
+@slash.slash(name="ping", description="Fyfan vad coolt", guild_ids=[377169144648302597])
+async def _ping(ctx):
+    await ctx.send(f"Pong! ({bot.latency * 1000}ms)")
+
+
+@slash.slash(name="test",
+             guild_ids=[377169144648302597],
+             description="This is just a test command, nothing more.",
+             options=[
+                 create_option(
+                     name="optone",
+                     description="This is the first option we have.",
+                     option_type=SlashCommandOptionType.STRING,
+                     required=False,
+                     choices=[
+                         create_choice(
+                             name="ChoiceOne",
+                             value="DOGE!"
+                         ),
+                         create_choice(
+                             name="ChoiceTwo",
+                             value="NO DOGE"
+                         )
+                     ]
+                 )
+             ])
+async def test(ctx, optone: str):
+    await ctx.send(content=f"Wow, you actually chose {optone}? :(")
 
 
 if __name__ == "__main__":
