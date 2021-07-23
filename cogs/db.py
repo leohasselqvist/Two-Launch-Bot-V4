7c407decb6f81380a4ea35f7f57fd119c23b97dd
Leo <leo.hasselqvist@elev.ntig.se>
2021-07-23 22:31:10 +0200

    Updated the adduser command to use slash functionality

--- cogs/db.py
+++ cogs/db.py
@@ -1,4 +1,8 @@
 from discord.ext import commands
+from discord import Embed
+from discord_slash import SlashCommand, SlashContext, cog_ext
+from discord_slash.utils.manage_commands import create_option, create_choice
+from discord_slash.model import SlashCommandOptionType
 import db
 
 
@@ -6,19 +10,35 @@ class DB(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.has_role(616590353709006859)
-    @commands.command()
-    async def adduser(self, ctx, new_user_id: int, new_user_name: str):
+    @commands.has_permissions(administrator=True)
+    @cog_ext.cog_slash(
+                        name="adduser",
+                        description="Add a new user to the database",
+                        guild_ids=[377169144648302597],
+                        options=[
+                            create_option(
+                                name="new_user_id",
+                                description="ID of the new user",
+                                option_type=SlashCommandOptionType.INTEGER,
+                                required=True),
+                            create_option(
+                                name="new_user_name",
+                                description="Nickname of the new user",
+                                option_type=SlashCommandOptionType.STRING,
+                                required=True)
+                        ])
+    async def adduser(self, ctx: SlashContext, new_user_id: int, new_user_name: str):
         db.createuser(new_user_id, new_user_name)
-        await ctx.send("Skapade en ny användare, " + new_user_name)
+        embed = Embed(title=f"User added", description=f"Added user '{new_user_name}' with ID {str(new_user_id)}'")
+        await ctx.send(embed=embed)
 
-    @commands.has_role(616590353709006859)
+    @commands.has_permissions(administrator=True)
     @commands.command()
     async def removeuser(self, ctx, user_id: int):
         db.deleteuser(userid=user_id)
         await ctx.send(f"Deleted user {user_id}")
 
-    @commands.has_role(616590353709006859)
+    @commands.has_permissions(administrator=True)
     @commands.command()
     async def viewuser(self, ctx, user_nick: str):
         user = db.fetchuser(0, user_nick)
@@ -27,7 +47,7 @@ class DB(commands.Cog):
         else:
             await ctx.send(f"ID: {user[0]}\nKB: {user[1]}\nNICK: {user[2]}")
 
-    @commands.has_role(616590353709006859)
+    @commands.has_permissions(administrator=True)
     @commands.command()
     async def edituser(self, ctx, user_id: int, user_column: str, new_value: str):
         if user_column == "KB":
