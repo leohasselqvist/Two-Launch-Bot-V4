17f47b86af135d7998988ddd1de4509386cc7822
Leo <leo.hasselqvist@elev.ntig.se>
2021-07-24 02:43:12 +0200

    Changed command input from int to str to take larger integers.

--- cogs/db.py
+++ cogs/db.py
@@ -19,7 +19,7 @@ class DB(commands.Cog):
                             create_option(
                                 name="new_user_id",
                                 description="ID of the new user",
-                                option_type=SlashCommandOptionType.INTEGER,
+                                option_type=SlashCommandOptionType.STRING,
                                 required=True),
                             create_option(
                                 name="new_user_name",
@@ -27,35 +27,93 @@ class DB(commands.Cog):
                                 option_type=SlashCommandOptionType.STRING,
                                 required=True)
                         ])
-    async def adduser(self, ctx: SlashContext, new_user_id: int, new_user_name: str):
-        db.createuser(new_user_id, new_user_name)
-        embed = Embed(title=f"User added", description=f"Added user '{new_user_name}' with ID {str(new_user_id)}'")
+    async def adduser(self, ctx: SlashContext, new_user_id: str, new_user_name: str):
+
+        if new_user_id.isdigit():
+            db.createuser(int(new_user_id), new_user_name)
+            embed = Embed(title=f"User added", description=f"Added user '{new_user_name}' with ID {str(new_user_id)}")
+        else:
+            embed = Embed(title="Operation Failed", description="ID must only consist of numbers")
         await ctx.send(embed=embed)
 
     @commands.has_permissions(administrator=True)
-    @commands.command()
-    async def removeuser(self, ctx, user_id: int):
-        db.deleteuser(userid=user_id)
-        await ctx.send(f"Deleted user {user_id}")
+    @cog_ext.cog_slash(
+        name="removeuser",
+        description="Remove an existing user from the database",
+        guild_ids=[377169144648302597],
+        options=[
+            create_option(
+                name="user_id",
+                description="ID of the user",
+                option_type=SlashCommandOptionType.STRING,
+                required=True)
+        ])
+    async def removeuser(self, ctx: SlashContext, user_id: str):
+        if user_id.isdigit():
+            db.deleteuser(userid=int(user_id))
+            embed = Embed(title=f"User removed", description=f"Removed user {user_id}")
+        else:
+            embed = Embed(title="Operation Failed", description="ID must only consist of numbers")
+        await ctx.send(embed=embed)
 
     @commands.has_permissions(administrator=True)
-    @commands.command()
+    @cog_ext.cog_slash(
+        name="viewuser",
+        description="View an existing user in the database",
+        guild_ids=[377169144648302597],
+        options=[
+            create_option(
+                name="user_nick",
+                description="Nickname of the user",
+                option_type=SlashCommandOptionType.STRING,
+                required=True)
+        ])
     async def viewuser(self, ctx, user_nick: str):
         user = db.fetchuser(0, user_nick)
         if not user:  # If the returned value from fetchuser() is null, the user does not exist
-            await ctx.send("Användaren finns ej")
+            embed = Embed(title="Could not find user", description=f"The user '{user_nick}' could not be found.")
         else:
-            await ctx.send(f"ID: {user[0]}\nKB: {user[1]}\nNICK: {user[2]}")
+            embed = Embed(title=user[2], description=f"**ID:** {user[0]}\n**KB:** {user[1]}\n")
+        await ctx.send(embed=embed)
 
     @commands.has_permissions(administrator=True)
-    @commands.command()
-    async def edituser(self, ctx, user_id: int, user_column: str, new_value: str):
+    @cog_ext.cog_slash(
+        name="edituser",
+        description="Add a new user to the database",
+        guild_ids=[377169144648302597],
+        options=[
+            create_option(
+                name="user_id",
+                description="ID of the user",
+                option_type=SlashCommandOptionType.STRING,
+                required=True),
+            create_option(
+                name="user_column",
+                description="Which attribute to edit",
+                option_type=SlashCommandOptionType.STRING,
+                required=True),
+            create_option(
+                name="new_value",
+                description="What the new value should be",
+                option_type=SlashCommandOptionType.STRING,
+                required=True)
+        ])
+    async def edituser(self, ctx, user_id: str, user_column: str, new_value: str):
+        if user_id.isdigit():
+            user_id = int(user_id)
+        else:
+            await ctx.send(
+                embed=Embed(title="Operation Failed", description="ID must only consist of numbers")
+            )
+            return
+
         if user_column == "KB":
             db.edituser(user_id, user_column, int(new_value))
         else:
             db.edituser(user_id, user_column, new_value)
 
-        await ctx.send(f"Ändrade {user_column} på {user_id} till {new_value}")
+        embed = Embed(title=f"Edited user", description=f"Changed '{user_column}' on ID {str(user_id)} to {new_value}")
+        await ctx.send(embed=embed)
 
 
 def setup(client):
